24352559ba903ca6856d5de085c8ae099b6458dd
TvdH-UX <thomasheuvel7@gmail.com>
2022-04-15 10:37:46 +0200

    Add reset, reinitialize sketching functionality
    Add a button to reset the sketching area, asking for a new value
    to create a grid from. Fix issue of sketching not working after reset

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
+const body = document.body;
+const header = document.createElement("header");
+body.prepend(header);
+
+const resetButton = document.createElement("button");
+resetButton.textContent = "Reset";
+header.appendChild(resetButton);
+
 const container = document.querySelector(".container");
 
-// Function to create a grid of 16x16
+function askValue(){
+    const value = parseInt(prompt("Enter a number to generate a grid"));
+    return value;
+}
 
-function generateGrid(number){
-    const totalSquares = number * number;
+// Function to create a grid of 16x16
+function generateGrid(value){
+    value = askValue();
+    const totalSquares = value * value;
+    console.log(totalSquares)
     for (let i = 0; i < totalSquares; i++){
         const block = document.createElement("div");
         block.classList.add("square");
@@ -11,12 +25,26 @@ function generateGrid(number){
     };
 };
 
-generateGrid(16);
-
-const blocks = document.querySelectorAll(".square");
-
-blocks.forEach((block) => {
-    block.addEventListener('mouseenter', () => {
-        block.classList.add("squareHovered");
+function initializeSketching(){
+    const blocks = document.querySelectorAll(".square");
+    blocks.forEach((block) => {
+        block.addEventListener('mouseenter', () => {
+            block.classList.add("squareHovered");
+        });
     });
+};
+
+generateGrid();
+initializeSketching();
+
+// Event listener to change the color of a square when moused over
+
+resetButton.addEventListener('click', () => {
+    const blocks = document.querySelectorAll(".square");
+    blocks.forEach((block) => {
+        block.remove();
+        
+    });
+    generateGrid();
+    initializeSketching();
 });
